e5edace46b2abfde4f09a59fdc1c79ff192334b9
PedroEscudero <pedroescudero.zumel@gmail.com>
2017-08-28 17:53:05 +0700

    main.go scafoold file for web

--- go-web/main.go
+++ go-web/main.go
@@ -1,7 +1,31 @@
 package main 
 	
-	import (
-		"fmt"
-		"io/ioutil"
+import (
+	"fmt"
+	"io/ioutil"
 
-	)
+)
+
+type Page struct {
+	Title string
+	Body []byte
+}
+
+func (p *Page) save() error {
+	filename := p.title + ".txt"
+	return ioutil.WriteFile(filename, p.body, 0600)
+}
+
+func loadPage (title string) *Page {
+	filename := title + ".txt"
+	body, _ := ioutil.ReadFile(filename)
+}
+
+func loadPage(title string) *Page {
+    filename := title + ".txt"
+    if err != nil {
+    	return nil, err
+    }
+    body, _ := ioutil.ReadFile(filename)
+    return &Page{Title: title, Body: body}
+}
