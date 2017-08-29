56f83c795e427549b2f372f1835ff4807b76620a
PedroEscudero <pedroescudero.zumel@gmail.com>
2017-08-29 20:38:31 +0700

    Added editor and save handlers

--- go-web/page.go
+++ go-web/page.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"html/template"
 )
 
 type Page struct {
@@ -30,13 +31,41 @@ func loadPage(title string) *Page {
     return &Page{Title: title, Body: body}, nil
 }
 
-func viewHandle (w http.ResponseWriter, r *http.request){
-	title := r.URL.Path[len("/view/")]
-	  p, _ := loadPage(title)
-    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/view/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+        return
+    }
+    renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/edit/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        p = &Page{Title: title}
+    }
+    renderTemplate(w, "view", p)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+    t, _ := template.ParseFiles(tmpl + ".html")
+    t.Execute(w, p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/save/"):]
+    body := r.FormValue("body")
+    p := &Page{Title: title, Body: []byte(body)}
+    p.save()
+    http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
 func main() {
+	http.HandleFunc("/edit/", editHandler)
+    http.HandleFunc("/save/", saveHandler)
     http.HandleFunc("/view/", viewHandler)
     http.ListenAndServe(":8080", nil)
 }
