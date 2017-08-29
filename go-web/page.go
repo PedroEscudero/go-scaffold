c744f6f423921584989f70965500a7afde749e93
PedroEscudero <pedroescudero.zumel@gmail.com>
2017-08-29 14:26:56 +0700

    Added server feature to main

--- go-web/page.go
+++ go-web/page.go
@@ -30,13 +30,14 @@ func loadPage(title string) *Page {
     return &Page{Title: title, Body: body}, nil
 }
 
-func main() {
-	p1 := &Page{Title: "TestPage", }
+func viewHandle (w http.ResponseWriter, r *http.request){
+	title := r.URL.Path[len("/view/")]
+	  p, _ := loadPage(title)
+    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
 func main() {
-    p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-    p1.save()
-    p2, _ := loadPage("TestPage")
-    fmt.Println(string(p2.Body))
+    http.HandleFunc("/view/", viewHandler)
+    http.ListenAndServe(":8080", nil)
 }
+
