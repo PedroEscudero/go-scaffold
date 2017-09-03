579e4e5b1a55b2495c7c00b522991995f3d6bb46
PedroEscudero <pedroescudero.zumel@gmail.com>
2017-09-03 23:57:00 +0200

    Remove bash add from config.sh

--- config.sh
+++ config.sh
@@ -8,6 +8,4 @@ sudo curl -LO https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz
 sudo tar -C /usr/local -xvzf go1.7.linux-amd64.tar.gz
 sudo yum -y install git
 mkdir -p ~/projects/{bin,pkg,src}
-
-source /etc/profile && source ~/.bash_profile
 echo 'Done!!'
