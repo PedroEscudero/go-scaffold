3861e5289d3405c553643ba25ceebe423360a200
PedroEscudero <pedroescudero.zumel@gmail.com>
2017-10-13 00:16:27 +0200

    Refactor main file

--- go-rest/restful.go
+++ go-rest/restful.go
@@ -1,17 +1,81 @@
 package main
 
 import (
-    "fmt"
-    "html"
-    "log"
-    "net/http"
+    "encoding/json"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	"fmt"
+	"strings"
 )
 
+var books []Book
+var	router = mux.NewRouter()
+
+type Author struct {
+    Name  string `json:"name,omitempty"`
+    Nacionality string `json:"nacionality,omitempty"`
+}
+
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Cua!  %q", html.EscapeString(r.URL.Path))
-    })
+    setRoutes()
+    seedBooks()
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
 
-    log.Fatal(http.ListenAndServe(":8080", nil))
+func seedBooks() {
+	books = append(books, Book{ID: "1", Title: "Bible", Pages: 1000, ISBN: "weweuruiererer-4", Author: &Author{Name: "God", Nacionality: "All"}})
+	books = append(books, Book{ID: "2", Title: "It", Pages: 500, ISBN: "weweuruiererer-5", Author: &Author{Name: "Sthepen King", Nacionality: "USA"}})
+	books = append(books, Book{ID: "3", Title: "Zombikindergarten", Pages: 250, ISBN: "miimimie3", Author: &Author{Name: "Pedro Escudero", Nacionality: "España"}})
+}
 
+func setRoutes(){
+	setRoute("book")
+}
+
+func setRoute(route string){
+	router.HandleFunc("/{route}", GetBooks).Methods("GET")
+	router.HandleFunc("/{route}/{id}", GetBook).Methods("GET")
+	router.HandleFunc("/{route}/{id}", CreateBook).Methods("POST")
+    router.HandleFunc("/{route}/{id}", Delete).Methods("DELETE")
+}
+
+func GetBooks(w http.ResponseWriter, r *http.Request) {
+	model := strings.Replace(r.URL.Path, "/", "", -1)
+	if model == "books"{
+		json.NewEncoder(w).Encode(books)
+	}else{
+		fmt.Fprintf(w, "Hello, %q", r.URL.Path)
+	}
+}
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+    for _, item := range books {
+        if item.ID == params["id"] {
+            json.NewEncoder(w).Encode(item)
+            return
+        }
+    }
+    json.NewEncoder(w).Encode(&Book{})
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+    var book Book
+    _ = json.NewDecoder(r.Body).Decode(&book)
+    book.ID = params["id"]
+    books = append(books, book)
+    json.NewEncoder(w).Encode(books)
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+    for index, item := range books {
+        if item.ID == params["id"] {
+            books = append(books[:index], books[index+1:]...)
+            break
+        }
+        json.NewEncoder(w).Encode(books)
+    }
 }
